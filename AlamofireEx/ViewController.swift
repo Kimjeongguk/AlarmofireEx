bb76b40ce808512c7b074413432ce62eb6ede905
KimJeongguk <mindstar7949@naver.com>
2021-08-19 23:56:51 +0900

    파싱 연습중

--- AlamofireEx/ViewController.swift
+++ AlamofireEx/ViewController.swift
@@ -6,14 +6,58 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        Alamofire.request("https://api.github.com/users", method: .get, parameters: [:], encoding: URLEncoding.default, headers: ["Content-Type":"application/json", "Accept":"application/json"])
+            .validate(statusCode: 200..<300)
+            .responseJSON { (response) in
+                if let jsonvalue = response.result.value {
+                    do{
+                        let data = try JSONSerialization.data(withJSONObject: jsonvalue, options: .prettyPrinted)
+                        let value = ViewController.parseUserInfo(data)
+                        print(value)
+                    }
+                    catch {
+                        
+                    }
+                }
+        }
+    }
+    
+    static func parseUserInfo(_ data: Data) -> [Values] {
+        let decoder = JSONDecoder()
+        
+        do {
+            let response = try decoder.decode(Response.self, from: data)
+            let user = response.value
+            print("******\(user)")
+            return user
+        }catch let error {
+            print("-->parsing error: \(error.localizedDescription)")
+            return []
+        }
     }
 
 
+}
+
+struct Response: Codable {
+    let resultCount: Int
+    let value: [Values]
+}
+struct Values: Codable {
+    let id: Int
+    let login: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case login
+    }
 }
 
